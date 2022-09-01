31ddf7206478ff0630d97733f47407eaedcf5a03
nakamura yugo <runworld1919@gmail.com>
2022-09-01 13:01:55 +0900

    css&Javascript修正

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // データ
-var result = "";
+let result = "";
 // =で計算したかどうか
-var is_calc = false;
+let is_calc = false;
 
 // 初期表示
 window.onload = function () {
@@ -45,7 +45,7 @@ function ope_click(val){
 function equal_click(){
   if(is_ope_last())  result.value = result.value.slice(0, -1);
 
-  var temp = new Function("return " + result.value.replaceAll("×", "*").replaceAll("÷", "/"))();
+  let temp = new Function("return " + result.value.replaceAll("×", "*").replaceAll("÷", "/"))();
   if(temp == Infinity || Number.isNaN(temp)){
     result.value = "Error";
   }else{
